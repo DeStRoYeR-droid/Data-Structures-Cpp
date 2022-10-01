eab4b4a23d7823edd83bc5409c02c9048431daa2
Immanuel Vivek <immanuelvivek40609@gmail.com>
2022-10-01 21:36:16 +0530

    Changed implementation of linked list

--- Data Structures/linkedList.cpp
+++ Data Structures/linkedList.cpp
@@ -17,202 +17,326 @@ class Node{
             this->value = value;
             next = NULL;
         }
+        
 
-        void setValue(int newVal){
-            this->value = newVal;
-        }
-
-        void setNext(int value){
-            Node* temp = new Node(value);
-            this->next = temp;
-        }
-
-        void setNext(Node* nextNode){
-            this->next = nextNode;
-        }
-
-        int getValue(){
-            return this->value;
-        }
-
-        Node* getNext(){
-            return this->next;
-        }
-    
-        friend class LinkedList;
+        friend class singlyLinkedList;
 };
 
-class LinkedList{
+class singlyLinkedList{
     private:
         Node* head;
         Node* tail;
+        int size;
 
     public:
-        LinkedList(){
+        // Blank constructor
+		singlyLinkedList(){
             head = tail = NULL;
+            size = 0;
         }
-
-        LinkedList(int value){
+        
+		// Parameterized constructor
+        singlyLinkedList(int value){
             Node* temp = new Node(value);
             head = tail = temp;
+            size = 1;
         }
+        
+        // Destructor
+        ~singlyLinkedList(){
+        	while (!empty()){
+        		removeHead();
+			}
+			
+			delete head, tail, size;
+		}
+		
+		int getSize() const { return size;}
+		
+		// Method to check if linked list is empty or not
+        bool empty() const{ return (head == NULL); }
 
-        LinkedList(const LinkedList& l1){
-            this->head = l1.head;
+		// Method to print the linked list
+        void print() const{
+            if (empty()) cout << "Empty list" << endl;
+            else{
+                Node* temp = head;
+                while (temp->next != NULL){
+                    cout << temp->value << "->";
+                    temp = temp->next;
+                }
+                cout << temp->value << endl;
+            }
         }
-
+        
+        //Method to add to the back of the list
+		void addBack(int value){
+			Node* temp = new Node(value);
+			
+			if (head == NULL) head = tail = temp;
+			else{
+				tail->next = temp;
+				tail = temp;
+			}
+			this->size++;
+		}
+		
+		// Method to add to the front of the list
         void addFront(int value){
             Node* temp = new Node(value);
             
-            if (head == NULL){
-                head = tail = temp;
-                return;
-            }
-
-            temp->next = head;
-            head = temp;
+            if (head == NULL) head = tail = temp;
+			else{
+            	temp->next = head;
+            	head = temp;
+        	}
+			this->size++;
+        }
+        
+        // Method to remove the head
+        void removeHead(){
+            if (empty()) throw "Underflow";
+            
+			Node* temp = head;
+            if (head == tail) head = tail = NULL;
+            else head = head->next;
+			            
+            delete temp;
+            size--;
         }
 
-        void addBack(int value){
-            Node* temp = new Node(value);
+        void removeTail(){
+        	if (empty()) throw "Underflow";
+        	
+			Node* p = head;
+        	if (head == tail) head = tail = NULL;
+        	else{				
+	        	while (p->next != tail){
+	        		p = p->next;
+				}
+				tail = p;
+				p = p->next;
+				tail->next = NULL;
+			}
+			
+			delete p;
+			size--;
+		}
+		
+		void removePos(int position){
+			if (empty()) throw "Underflow";
+			if (position < 0 || position > size) throw "PositionError";
+			
+			if (position == 1) removeHead();
+			else if (position == size) removeTail();
+			else{
+				Node* temp = head;
+				Node* trailer = NULL;
+				int cur_pos = 1;
+				while (cur_pos != position){
+					trailer = temp;
+					temp = temp->next;
+					cur_pos++;
+				}
+				trailer->next = temp->next;
+				delete temp;
+				size--;
+			}
+		}
+		
+		void insertPos(int value , int position){
+			if (position < 0 || position-1 > size) throw "PositionError";
+			
+			if (position == 1) addFront(value);
+			else if (position == size+1) addBack(value);
+			else{
+				Node* p = new Node(value);
+				Node* temp = head;
+				int cur_pos = 1;
+				while (cur_pos != position - 1){
+					temp = temp->next;	
+					cur_pos++;
+				}
+				p->next = temp->next;
+				temp->next = p;
+				size++;
+			}
+		}
 
-            if (tail == NULL){
-                head = tail = temp;
-            }
-
-            tail->next = temp;
-            tail = temp;
-        }
-
-        void print(){
-            Node* temp = head;
-            while (temp->next != NULL){
-                cout << temp->value << "->";
-                temp = temp->next;
-            }
-            cout << temp->value << endl;
-        }
-
-        int getLength() const{
-            if (head == NULL){
-                return 0;
-            }
-
-            int length = 1;
-            Node* temp = head;
-            while (temp->next != NULL){
-                length++;
-                temp = temp->next;
-            }
-
-            return length;
-        }
-
-        int getValue(int pos) const {
-            if (pos > getLength()) throw "OutOfBoundsException";
-            if (pos == 1) return head->value;
-
-            int cur_pos = 1;
-            Node* temp = head;
-            while (cur_pos != pos){
-                temp = temp->next;
-                cur_pos++;
-            }
-            return temp->value;
-        }
-
-        LinkedList merge(const LinkedList& l1){
-            int chosen_this = 1;
-            int chosen_oth = 1;
-
-            LinkedList temp;
-
-            while (chosen_this <= getLength() && chosen_oth <= l1.getLength()){
-                int val_this = getValue(chosen_this);
-                int val_oth = l1.getValue(chosen_oth);
-
-                if (val_this > val_oth){
-                    temp.addBack(val_oth);
-                    chosen_oth++;
+        int getValue(int position) const {
+            if (position < 0 || position-1 > size) throw "InvalidPositionError";
+            else{
+                int cur_pos = 1;
+                Node* temp = head;
+                while (cur_pos < position){
+                    temp = temp->next;
+                    cur_pos++;
                 }
 
-                else {
-                    temp.addBack(val_this);
-                    chosen_this++;
+                return temp->value;
+            }
+        }
+		
+		void concatenate(singlyLinkedList& other){
+			if (empty() && other.empty()) return;
+			else if (other.empty()) return;
+			else if (empty()){
+				this->head = other.head;
+				this->tail = other.tail;
+				this->size = other.size;
+			}
+			else{
+				this->tail->next = other.head;
+				this->tail = other.tail;
+				this->size += other.size;
+			}
+			other.head = other.tail = NULL;
+            other.size = 0;
+		}
+		
+		Node* search(int value) const{
+            Node* temp = head;
+            while (temp != NULL && temp->value != value){
+                temp = temp->next;
+            }
+            
+            if (temp) return temp;
+            return NULL;
+        }
+};
+
+class sortedSinglyLinkedList : private singlyLinkedList{
+	public:
+        // Declaring the members of parent class which are to be made public
+	 	using singlyLinkedList::removeHead;
+		using singlyLinkedList::removeTail;
+		using singlyLinkedList::getValue;
+        using singlyLinkedList::getSize;
+        using singlyLinkedList::print;
+        using singlyLinkedList::search;
+    
+        sortedSinglyLinkedList() : singlyLinkedList(){}
+
+        sortedSinglyLinkedList(int value) : singlyLinkedList(value){}
+
+        void insert(int value){
+            if (empty()) addFront(value);
+            else{
+                for (int i = 1; i <= getSize(); i++){
+                    if (getValue(i) > value){
+                        insertPos(value , i);
+                        return;
+                    }
+                }
+                addBack(value);
+            }
+        }
+
+        // Deletes first instance of value
+        void deleteValue(int value){
+            if (empty()) throw "Underflow";
+            bool found = false;
+            for (int i = 1; i <= getSize(); i++){
+                if (getValue(i) == value){
+                    found = true;
+                    removePos(i);
+                    break;
+                }
+                else if (getValue(i) > value) break;
+            }
+            if (!found) throw "ValueNotFoundException";
+        }
+
+
+        sortedSinglyLinkedList merge(const sortedSinglyLinkedList& oth){
+            int thisPos = 1;
+            int othPos = 1;
+
+            sortedSinglyLinkedList temp;
+            
+            while (thisPos <= getSize() && othPos <= oth.getSize()){ 
+                if (getValue(thisPos) == oth.getValue(othPos)){
+                    temp.addBack(getValue(thisPos));
+                    thisPos++;
+                    othPos++;
+                }
+                else if (getValue(thisPos) < oth.getValue(othPos)){
+                    temp.addBack(getValue(thisPos));
+                    thisPos++;
+                }
+                else{
+                    temp.addBack(oth.getValue(othPos));
+                    othPos++;
                 }
             }
 
-            if (chosen_this <= getLength()){
-                while (chosen_this <= getLength()){
-                    temp.addBack(getValue(chosen_this));
-                    chosen_this++;
+            if (thisPos <= getSize()){
+                while (thisPos <= getSize()){
+                    temp.addBack(getValue(thisPos));
+                    thisPos++;
                 }
             }
 
-            if (chosen_oth <= l1.getLength()){
-                while (chosen_oth <= getLength()){
-                    temp.addBack(l1.getValue(chosen_oth));
-                    chosen_oth++;
+            if (othPos <= oth.getSize()){
+                while (othPos <= oth.getSize()){
+                    temp.addBack(oth.getValue(othPos));
+                    othPos++;
                 }
             }
 
             return temp;
         }
-
-        void deleteNode(int position){
-            if (position > getLength()) throw "OutOfBoundsException";
-            if (position == 1){
-                Node* temp = head;
-                head = head->next;
-                delete temp;
-                return;
-            }
-
-            int cur_pos = 1;
-            Node* trailer;
-            Node* temp = head;
-            while (cur_pos < position){
-                trailer = temp;
-                temp = temp->next;
-                cur_pos++;
-            }
-            
-            trailer->next = temp->next;
-            delete temp;
-        }
-        
-        void deleteNode(const LinkedList& oth){
-            Node* temp = oth.head;
-            int deleted = 0;
-
-            while (temp != NULL){
-                deleteNode(temp->value - deleted);
-                deleted++;
-                temp = temp->next;
-            }
-
-            return;
-        }
 };
 
 int main(){
-    LinkedList l1(20);
-    l1.addFront(10);
-    l1.addBack(30);
-    l1.addBack(40);
-    l1.addBack(50);
-    l1.addBack(60);
-    l1.addBack(70);
+	try{
+        /*
+		sortedSinglyLinkedList l1;
+        l1.insert(14);
+        l1.insert(17);
+        l1.insert(12);
+        l1.insert(12);
+        l1.insert(1);
+        l1.insert(27);
+        l1.print();
+        l1.deleteValue(12);
+        l1.deleteValue(12);
+        l1.print();
 
-    l1.print();
+        sortedSinglyLinkedList l2;
+        l2.insert(24);
+        l2.insert(94);
+        l2.insert(21);
+        l2.insert(42);
+        l2.insert(1);
+        l2.removeTail();
+        l2.print();
 
-    LinkedList l2;
-    l2.addBack(1);
-    l2.addBack(3);
-    l2.addBack(5);
-    l2.addBack(6);
-    
-    l1.deleteNode(l2);
-    l1.print();
-    return 0;
+        sortedSinglyLinkedList l3 = l1.merge(l2);
+        l3.print();
+        */
+        
+
+       /*
+        singlyLinkedList l1;
+        l1.addFront(12);
+        l1.addFront(75);
+        l1.addFront(93);
+        l1.addFront(31);
+        l1.insertPos(18 , 3);
+
+        singlyLinkedList l2;
+        l2.addFront(91);
+        l2.addBack(100);
+
+        l1.concatenate(l2);
+
+        l1.print();
+        l2.print();
+        */
+
+	} catch (const char* exp){
+		cout << "Exception occurred : " << exp << endl;
+	}
+	return 0;
 }
